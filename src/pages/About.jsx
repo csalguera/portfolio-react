3350c45db3f799b7cd7db6af091b28409a8b5826
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-27 23:26:18 -0500

    add some background to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,17 @@ function About() {
   return (
     <>
       <h1>About</h1>
+      <img src="https://i.imgur.com/H5qSmJf.jpg" alt="" />
+      <p>Former Chef turned Software Engineer</p>
+      <p>Originally a Californian from San Diego</p>
+      <p>Now a Hoosier from Indianapolis</p>
+      <p>Passions include:</p>
+      <ul>
+        <li>Cooking</li>
+        <li>Programming</li>
+        <li>Board games</li>
+        <li>Video games</li>
+      </ul>
     </>
   )
 }
