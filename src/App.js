81bd102e4feb8284eab7dfee94a65db7bb938329
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-27 22:34:16 -0500

    import Routes and Route to App.js along with all pages components and Route them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Landing from './pages/Landing'
+import Projects from './pages/Projects'
+import Resume from './pages/Resume'
 
 function App() {
   return (
     <>
       <h1>App</h1>
+      <Routes>
+        <Route path={'/about'} element={<About />} />
+        <Route path={'/contact'} element={<Contact />} />
+        <Route path={'/'} element={<Landing />} />
+        <Route path={'/projects'} element={<Projects />} />
+        <Route path={'/resume'} element={<Resume />} />
+      </Routes>
     </>
   )
 }
