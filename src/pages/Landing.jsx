d4a4f091e5b61d70533682769a88c78cc116b4d0
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-27 22:45:27 -0500

    add h1, h2, and img tags to Landing page and apply minor styling to img

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,7 +3,9 @@ import '../styles/Landing.css'
 function Landing() {
   return (
     <>
-      <h1>Landing</h1>
+      <h1>Carlos Salguera</h1>
+      <h2>Fullstack Developer</h2>
+      <img src="https://i.imgur.com/H5qSmJf.jpg" alt="" />
     </>
   )
 }
