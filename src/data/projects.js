1bb54b79ab179c60ac0e56b231fa70e0d3415625
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-28 13:10:09 -0500

    make data directory and touch projects.js file

---
+++ src/data/projects.js
@@ -0,0 +1,16 @@
+export const projects = [
+  {
+    title: 'Connect Four',
+    description: 'An app that recreates the classic game of Connect Four. Play against another player locally.',
+    image: 'https://i.imgur.com/rI6e29W.jpg',
+    repositoryLink: 'https://github.com/csalguera/connect-four',
+    deploymentLink: 'https://csalguera-connect-four.netlify.app/',
+  },
+  {
+    title: 'garner',
+    description: 'An app that recreates the classic game of Connect Four. Play against another player locally.',
+    image: 'https://i.imgur.com/gP3hltL.png',
+    repositoryLink: 'https://github.com/csalguera/garner',
+    deploymentLink: 'https://garner-game-collector.fly.dev/',
+  },
+]
