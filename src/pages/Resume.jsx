be0e8ce75d380d8a2e5714282c9d273335df8fbe
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-28 01:42:37 -0500

    add anchor tags to view or download my resume in Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,6 +4,9 @@ function Resume() {
   return (
     <>
       <h1>Resume</h1>
+      <p>
+        View <a href="/carlos-salguera-resume.pdf" target="_blank">here</a> or download <a href="/carlos-salguera-resume.pdf" download>here.</a>
+      </p>
     </>
   )
 }
