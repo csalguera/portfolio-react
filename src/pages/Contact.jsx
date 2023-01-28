310ba5afb0e233828b42d06790cc1c249e64fe7a
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-27 23:54:13 -0500

    add Links to GitHub and LinkedIn within Contact page as well as a mailto a tag for email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom'
 import '../styles/Contact.css'
 
 function Contact() {
   return (
     <>
       <h1>Contact</h1>
+      <p>
+        Reach out to me via <a href="mailto:carlos.e.salguera@gmail.com">email</a>
+      </p>
+      <p>Alternatively via:</p>
+      <ul>
+        <li><Link to="https://www.linkedin.com/in/carlos-salguera/" target="_blank">LinkedIn</Link></li>
+        <li><Link to="https://github.com/csalguera" target="_blank">GitHub</Link></li>
+      </ul>
     </>
   )
 }
