ea767ce55da0feda2da70e644782dd5eff9d93ce
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-27 22:39:47 -0500

    add links to pages components

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <>
+      <nav>
+        <Link to='/'>Carlos Salguera</Link>
+        <ul>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
+          <li><Link to='/projects'>Projects</Link></li>
+          <li><Link to='/resume'>Resume</Link></li>
+        </ul>
+      </nav>
     </>
   )
 }
