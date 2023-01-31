72374829b6d351b297cc67177a363a6954599e9b
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-31 17:24:19 -0500

    add h3 tag to Landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,6 +6,7 @@ function Landing() {
       <h1>Carlos Salguera</h1>
       <h2>Fullstack Developer</h2>
       <img className='landing-img' src="https://i.imgur.com/H5qSmJf.jpg" alt="" />
+      <h3>JavaScript | MERN | CSS</h3>
     </>
   )
 }
