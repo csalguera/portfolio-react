81db99f3fd19ee0a0cd6e11ec875eed16a36533d
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-03-04 14:21:34 -0500

    add new object to data/projects.js file

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,10 @@
 export const projects = [
   {
-    title: 'Connect Four',
-    description: 'An app that recreates the classic game of Connect Four. Play against another player locally.',
-    image: 'https://i.imgur.com/rI6e29W.jpg',
-    repositoryLink: 'https://github.com/csalguera/connect-four',
-    deploymentLink: 'https://csalguera-connect-four.netlify.app/',
+    title: 'log-go',
+    description: 'An app where users can create collections of their favorite movies and books.',
+    image: 'https://i.imgur.com/5jQAOqg.png" width="800px',
+    repositoryLink: 'https://github.com/csalguera/log-go-front-end',
+    deploymentLink: 'https://log-go-csalguera.netlify.app/',
   },
   {
     title: 'garner',
@@ -12,5 +12,12 @@ export const projects = [
     image: 'https://i.imgur.com/gP3hltL.png',
     repositoryLink: 'https://github.com/csalguera/garner',
     deploymentLink: 'https://garner-game-collector.fly.dev/',
+  },
+  {
+    title: 'Connect Four',
+    description: 'An app that recreates the classic game of Connect Four. Play against another player locally.',
+    image: 'https://i.imgur.com/rI6e29W.jpg',
+    repositoryLink: 'https://github.com/csalguera/connect-four',
+    deploymentLink: 'https://csalguera-connect-four.netlify.app/',
   },
 ]
