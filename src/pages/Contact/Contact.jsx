594ee6b46bf19c64260d662b287521fb1a8cd69a
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-03-08 02:10:13 -0500

    update styling

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,3 +1,4 @@
+import styles from './Contact.module.css'
 import { Link } from 'react-router-dom'
 
 function Contact({ contactRef }) {
@@ -5,16 +6,48 @@ function Contact({ contactRef }) {
     <main className='page-component-container' ref={contactRef}>
       <h1>Contact</h1>
       <p>
-        Connect with me via <a href="mailto:carlos.e.salguera@gmail.com">email</a>
+        {'Connect with me via '}
+        <Link
+        to=''
+        onClick={() => window.location = 'mailto:carlos.e.salguera@gmail.com'}
+        className={styles["link-tag"]}
+        >
+          email
+        </Link>
       </p>
       <p>Alternatively via:</p>
-      <ul>
-        <li><Link to="https://www.linkedin.com/in/carlos-salguera/" target="_blank">LinkedIn</Link></li>
-        <li><Link to="https://github.com/csalguera" target="_blank">GitHub</Link></li>
-      </ul>
+      <Link
+        to="https://www.linkedin.com/in/carlos-salguera/"
+        target="_blank"
+        className={styles["link-tag"]}
+      >
+        LinkedIn
+      </Link>
+      <Link
+        to="https://github.com/csalguera"
+        target="_blank"
+        className={styles["link-tag"]}
+      >
+        GitHub
+      </Link>
       <h1>Resume</h1>
       <p>
-        View <a href="/carlos-salguera-resume.pdf" target="_blank">here</a> or download <a href="/carlos-salguera-resume.pdf" download>here.</a>
+        {'View '}
+        <Link
+          to="/carlos-salguera-resume.pdf"
+          target="_blank"
+          className={styles["link-tag"]}
+        >
+          here
+        </Link>
+        {' or download '}
+        <Link
+        to="/carlos-salguera-resume.pdf"
+        download
+        className={styles["link-tag"]}
+        >
+          here
+        </Link>
       </p>
     </main>
   )
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,11 +2,13 @@ import styles from './Landing.module.css'
 
 function Landing({ landingRef }) {
   return (
-    <main className='page-component-container' ref={landingRef}>
-      <h1>Carlos Salguera</h1>
-      <h2>Fullstack Developer</h2>
-      <img className={styles['landing-img']} src="https://i.imgur.com/H5qSmJf.jpg" alt="" />
-      <h3>TypeScript | PERN | MERN | JavaScript | CSS</h3>
+    <main ref={landingRef}>
+      <div className={styles["landing-details"]}>
+        <h1>Carlos Salguera</h1>
+        <h2>Fullstack Developer</h2>
+        <img className={styles['landing-img']} src="https://i.imgur.com/H5qSmJf.jpg" alt="" />
+        <h3>TypeScript | PERN | MERN | JavaScript | CSS</h3>
+      </div>
     </main>
   )
 }
