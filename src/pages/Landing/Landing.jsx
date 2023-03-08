f24d54a9f15efb9ccb7471577086a0de7ca39cfc
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-03-08 04:36:02 -0500

    stub up Footer component

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,12 +2,14 @@ import styles from './Landing.module.css'
 
 function Landing({ landingRef }) {
   return (
-    <main ref={landingRef} className={styles.container}>
+    <main ref={landingRef} style={{backgroundColor: '#bababa'}}>
       <div className={styles["landing-details"]}>
-        <h1>Carlos Salguera</h1>
-        <h2>Fullstack Developer</h2>
-        <img className={styles['landing-img']} src="https://i.imgur.com/H5qSmJf.jpg" alt="" />
-        <h3>TypeScript | PERN | MERN | JavaScript | CSS</h3>
+        <div className={styles.container}>
+          <h1>Carlos Salguera</h1>
+          <h2>Fullstack Developer</h2>
+          <img className={styles['landing-img']} src="https://i.imgur.com/H5qSmJf.jpg" alt="" />
+          <h3>TypeScript | PERN | MERN | JavaScript | CSS</h3>
+        </div>
       </div>
     </main>
   )
