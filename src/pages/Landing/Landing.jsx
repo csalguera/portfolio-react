e2eab5e588d6135cea687bca57845eed893d6d0b
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-03-12 22:59:07 -0400

    change image src

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,7 +8,7 @@ function Landing({ landingRef }) {
         <div className={styles.container}>
           <h1>Carlos Salguera</h1>
           <h2>Fullstack Developer</h2>
-          <img className={styles['landing-img']} src="https://i.imgur.com/H5qSmJf.jpg" alt="" />
+          <img className={styles['landing-img']} src="https://i.imgur.com/Rd1RywH.jpg" alt="" />
           <h3>TypeScript | PERN | MERN | JavaScript | CSS</h3>
         </div>
       </div>
