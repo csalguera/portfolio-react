e2f97c030a93af0d98084f7d551558750f431ad4
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-03-21 17:10:16 -0400

    fix indentation

--- src/components/about/Skills/Skills.jsx
+++ src/components/about/Skills/Skills.jsx
@@ -11,41 +11,41 @@ const Skills = () => {
   return (
     <>
       <h2>Skills</h2>
-        <div className={styles["skills-container"]}>
-          <div className={styles["front-end"]}>
-            <h3>Front End</h3>
-            {skills.map(layer => (
-                layer.frontend.map(skill => (
-                  <Skill
-                    key={skill.name}
-                    skill={skill}
-                  />
-                ))
-            ))}
-          </div>
-          <div className={styles["back-end"]}>
-            <h3>Back End</h3>
-            {skills.map(layer => (
-              layer.backend.map(skill => (
+      <div className={styles["skills-container"]}>
+        <div className={styles["front-end"]}>
+          <h3>Front End</h3>
+          {skills.map(layer => (
+              layer.frontend.map(skill => (
                 <Skill
                   key={skill.name}
                   skill={skill}
                 />
               ))
-            ))}
-          </div>
-          <div className={styles["other"]}>
-            <h3>Other</h3>
-            {skills.map(layer => (
-              layer.other.map(skill => (
-                <Skill
-                  key={skill.name}
-                  skill={skill}
-                />
-              ))
-            ))}
-          </div>
+          ))}
         </div>
+        <div className={styles["back-end"]}>
+          <h3>Back End</h3>
+          {skills.map(layer => (
+            layer.backend.map(skill => (
+              <Skill
+                key={skill.name}
+                skill={skill}
+              />
+            ))
+          ))}
+        </div>
+        <div className={styles["other"]}>
+          <h3>Other</h3>
+          {skills.map(layer => (
+            layer.other.map(skill => (
+              <Skill
+                key={skill.name}
+                skill={skill}
+              />
+            ))
+          ))}
+        </div>
+      </div>
     </>
   )
 }
