13c0cbe282d20b5ad52ef411a97657b35f238512
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-04 16:47:23 -0400

    add img tag and make Icons component

---
+++ src/components/contact/Icons/Icons.jsx
@@ -0,0 +1,50 @@
+// mui components
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link'
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import EmailIcon from '@mui/icons-material/Email';
+
+const Icons = (props) => {
+  const {
+    fontColor1,
+  } = props
+  
+  return (
+    <Box sx={{ pt: '5vh' }}>
+      <Tooltip title="GitHub">
+        <IconButton>
+          <Link href='https://github.com/csalguera' target='_blank' rel='noreferrer'>
+            <GitHubIcon  fontSize='large' sx={{ color: fontColor1 }} />
+          </Link>
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="LinkedIn">
+        <IconButton>
+          <Link href='https://www.linkedin.com/in/carlos-salguera/' target='_blank' rel='noreferrer'>
+            <LinkedInIcon  fontSize='large' sx={{ color: fontColor1 }} />
+          </Link>
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Email">
+        <IconButton>
+          <Link href='mailto:carlos.e.salguera@gmail.com' target='_blank' rel='noreferrer'>
+            <EmailIcon  fontSize='large' sx={{ color: fontColor1 }} />
+          </Link>
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Resume">
+        <IconButton>
+          <Link href='/carlos-salguera.pdf' download>
+            <InsertDriveFileIcon  fontSize='large' sx={{ color: fontColor1 }} />
+          </Link>
+        </IconButton>
+      </Tooltip>
+    </Box>
+  )
+}
+
+export default Icons
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,17 +3,10 @@ import { useState } from 'react';
 
 // components
 import EmailForm from '../../components/contact/EmailForm/EmailForm'
+import Icons from '../../components/contact/Icons/Icons';
 
 // mui components
 import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link'
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
-import EmailIcon from '@mui/icons-material/Email';
 
 function Contact(props) {
   const {
@@ -25,37 +18,21 @@ function Contact(props) {
       <Typography variant="h2" sx={{ color: fontColor1 }}>
         Contact
       </Typography>
-      <Box sx={{ pt: '5vh' }}>
-        <Tooltip title="GitHub">
-          <IconButton>
-            <Link href='https://github.com/csalguera' target='_blank' rel='noreferrer'>
-              <GitHubIcon  fontSize='large' sx={{ color: fontColor1 }} />
-            </Link>
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="LinkedIn">
-          <IconButton>
-            <Link href='https://www.linkedin.com/in/carlos-salguera/' target='_blank' rel='noreferrer'>
-              <LinkedInIcon  fontSize='large' sx={{ color: fontColor1 }} />
-            </Link>
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Email">
-          <IconButton>
-            <Link href='mailto:carlos.e.salguera@gmail.com' target='_blank' rel='noreferrer'>
-              <EmailIcon  fontSize='large' sx={{ color: fontColor1 }} />
-            </Link>
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Resume">
-          <IconButton>
-            <Link href='/carlos-salguera.pdf' download>
-              <InsertDriveFileIcon  fontSize='large' sx={{ color: fontColor1 }} />
-            </Link>
-          </IconButton>
-        </Tooltip>
-      </Box>
+      <img
+        src='https://i.imgur.com/Rd1RywH.jpg'
+        alt=''
+        style={{
+          maxWidth: '300px',
+          maxHeight: '300px',
+          objectFit: 'cover',
+          borderRadius: '50%',
+          marginTop: '5vh',
+        }}
+      />
       <EmailForm
+        fontColor1={fontColor1}
+      />
+      <Icons
         fontColor1={fontColor1}
       />
     </main>
