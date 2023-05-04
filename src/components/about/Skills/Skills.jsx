26c7fad185cb6cbe62add587fc9508f694985ff1
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-03 22:25:02 -0400

    fix indentation

--- src/components/about/Skills/Skills.jsx
+++ src/components/about/Skills/Skills.jsx
@@ -15,80 +15,80 @@ const Skills = (props) => {
 
   return (
     <>
-    <Typography variant="h4" sx={{ color: fontColor1, pt: '5vh' }}>
-      Skills
-    </Typography>
-    <Box sx={{ textAlign: 'center', display: 'flex' }}>
-      <Box
-        sx={{
-          pt: '2vh',
-          display: 'flex',
-          flexWrap: 'wrap',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'flex-start',
-          px: 4
-        }}
-      >
+      <Typography variant="h4" sx={{ color: fontColor1, pt: '5vh' }}>
+        Skills
+      </Typography>
+      <Box sx={{ textAlign: 'center', display: 'flex' }}>
+        <Box
+          sx={{
+            pt: '2vh',
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+            px: 4
+          }}
+        >
+          <Typography variant="h6" sx={{ color: fontColor1 }}>
+            Frontend
+          </Typography>
+          {skills.map(tech => (
+            tech.frontend.map(skill => (
+              <Skill
+                key={skill.name}
+                skill={skill}
+              />
+            ))
+          ))}
+        </Box>
+        <Box
+          sx={{
+            pt: '2vh',
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+            px: 4
+          }}
+        >
         <Typography variant="h6" sx={{ color: fontColor1 }}>
-          Frontend
-        </Typography>
-        {skills.map(tech => (
-          tech.frontend.map(skill => (
-            <Skill
-              key={skill.name}
-              skill={skill}
-            />
-          ))
-        ))}
+            Backend
+          </Typography>
+          {skills.map(tech => (
+            tech.backend.map(skill => (
+              <Skill
+                key={skill.name}
+                skill={skill}
+              />
+            ))
+          ))}
+        </Box>
+        <Box
+          sx={{
+            pt: '2vh',
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+            px: 4
+          }}
+        >
+        <Typography variant="h6" sx={{ color: fontColor1 }}>
+            Other
+          </Typography>
+          {skills.map(tech => (
+            tech.other.map(skill => (
+              <Skill
+                key={skill.name}
+                skill={skill}
+              />
+            ))
+          ))}
+        </Box>
       </Box>
-      <Box
-        sx={{
-          pt: '2vh',
-          display: 'flex',
-          flexWrap: 'wrap',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'flex-start',
-          px: 4
-        }}
-      >
-      <Typography variant="h6" sx={{ color: fontColor1 }}>
-          Backend
-        </Typography>
-        {skills.map(tech => (
-          tech.backend.map(skill => (
-            <Skill
-              key={skill.name}
-              skill={skill}
-            />
-          ))
-        ))}
-      </Box>
-      <Box
-        sx={{
-          pt: '2vh',
-          display: 'flex',
-          flexWrap: 'wrap',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'flex-start',
-          px: 4
-        }}
-      >
-      <Typography variant="h6" sx={{ color: fontColor1 }}>
-          Other
-        </Typography>
-        {skills.map(tech => (
-          tech.other.map(skill => (
-            <Skill
-              key={skill.name}
-              skill={skill}
-            />
-          ))
-        ))}
-      </Box>
-    </Box>
     </>
   )
 }
