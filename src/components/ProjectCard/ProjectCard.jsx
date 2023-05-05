e3d5afce3e85f1fdb6025c1e391adc1b5885aab1
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-05 15:52:56 -0400

    import MUI Popover component

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,13 +1,33 @@
+// npm modules
+import { useState } from 'react';
+
 // mui components
 import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Box, Button, CardActionArea, CardActions } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
+import Popover from '@mui/material/Popover';
 
 const ProjectCard = (props) => {
   const { project } = props
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
     <Card sx={{ maxWidth: 400, mx: '2vw', mb: 4 }}>
@@ -17,7 +37,28 @@ const ProjectCard = (props) => {
           height="200"
           image={project.image}
           alt={project.title}
+          onClick={handleClick}
         />
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'center',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'center',
+            horizontal: 'center',
+          }}
+        >
+          <img
+            src={project.image}
+            alt={project.title}
+            width="800px"
+          />
+        </Popover>
         <CardContent sx={{ height: 180 }}>
           <Typography gutterBottom variant="h5" component="div">
             {project.title}
