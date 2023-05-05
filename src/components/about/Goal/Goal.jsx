469371618ea8fb9b7a9ce9defad901f186dfada2
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-05 18:36:58 -0400

    fix indentation

--- src/components/about/Goal/Goal.jsx
+++ src/components/about/Goal/Goal.jsx
@@ -9,19 +9,19 @@ const Goal = () => {
         Goal
       </Typography>
       <Typography sx={{ pt: 2, px: 8 }}>
-      Software Engineer focused on <b>Full Stack</b> Development
+        Software Engineer focused on <b>Full Stack</b> Development
       </Typography>
       <Typography sx={{ pt: 2, px: 8 }}>
-      Currently seeking <b>full-time</b> opportunity in <b>Indianapolis, IN</b> or <b>Remote</b>
+        Currently seeking <b>full-time</b> opportunity in <b>Indianapolis, IN</b> or <b>Remote</b>
       </Typography>
       <Typography sx={{ pt: 2, px: 8 }}>
-      Recently graduated from a <b>full-time, 480+ hour, 12-week Software Engineering Immersive</b> at General Assembly
+        Recently graduated from a <b>full-time, 480+ hour, 12-week Software Engineering Immersive</b> at General Assembly
       </Typography>
       <Typography sx={{ pt: 2, px: 8 }}>
-      Previous work background ingrained a <b>strong work ethic</b> and established <b>discipline</b>
+        Previous work background ingrained a <b>strong work ethic</b> and established <b>discipline</b>
       </Typography>
       <Typography sx={{ pt: 2, px: 8 }}>
-      Studied Electrical Engineering at IvyTech Community College
+        Studied Electrical Engineering at IvyTech Community College
       </Typography>
       <Typography sx={{ pt: 2, px: 8 }}>
         Education paved the way to develop <b>problem-solving skills</b>
