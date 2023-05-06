a4507f34e6e9f70314d9ec771d85bd866c1c5a6d
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-06 00:59:51 -0400

    add Backdrop when email is sent

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -8,9 +8,12 @@ import SuccessAlert from '../../components/contact/SuccessAlert/SuccessAlert';
 
 // mui components
 import Typography from '@mui/material/Typography'
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function Contact() {
   const [emailSent, setEmailSent] = useState(false)
+  const [backdrop, setBackdrop] = useState(false)
 
   return (
     <main id='contact' className='page-component-container'>
@@ -30,12 +33,20 @@ function Contact() {
       />
       <EmailForm
         setEmailSent={setEmailSent}
+        setBackdrop={setBackdrop}
       />
       <Icons />
       <SuccessAlert
         emailSent={emailSent}
         setEmailSent={setEmailSent}
       />
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={backdrop}
+        // onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </main>
   )
 }
