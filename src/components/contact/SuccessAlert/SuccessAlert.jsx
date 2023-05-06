73054c56a1d81e4600bb0b8538edaab075973f9d
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-06 00:16:51 -0400

    add Zoom transition

--- src/components/contact/SuccessAlert/SuccessAlert.jsx
+++ src/components/contact/SuccessAlert/SuccessAlert.jsx
@@ -1,48 +1,36 @@
-// npm modules
-import { useEffect, useState } from 'react';
-
 // mui components
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
+import Zoom from '@mui/material/Zoom';
 
 const SuccessAlert = (props) => {
   const { emailSent, setEmailSent } = props
-  const [displayAlert, setDisplayAlert] = useState(false)
 
   const handleClose = () => {
-    setDisplayAlert(false)
-    setEmailSent(false)
+    setEmailSent(!emailSent)
   }
 
-  useEffect(() => {
-    if (emailSent) {
-      setDisplayAlert(true)
-    }
-  }, [emailSent])
-
   return (
-    <>
-      {displayAlert &&
-      <Stack
-        sx={{
-          mt: '4vh',
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
-        }}
-        spacing={2}
+  <Zoom in={emailSent}>
+    <Stack
+      sx={{
+        mt: '4vh',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+      spacing={2}
+    >
+      <Alert
+        onClose={handleClose}
+        sx={{ minWidth: '200px' }}
+        variant='filled'
+        color='primary'
       >
-        <Alert
-          onClose={handleClose}
-          sx={{ minWidth: '200px' }}
-          variant='filled'
-          color='primary'
-        >
-          Email sent!
-        </Alert>
-      </Stack>
-      }
-    </>
+        Email sent!
+      </Alert>
+    </Stack>
+  </Zoom>
   )
 }
 
