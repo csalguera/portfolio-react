969e5894d3b1220a6d7589d9270c4a8f8da222c3
csalguera <carlos.e.salguera@gmail.com>
2026-09-15 22:01:57 -0400

    update skills section

--- src/components/about/Skills/Skills.jsx
+++ src/components/about/Skills/Skills.jsx
@@ -1,50 +1,49 @@
 // data
-import { skills } from "../../../data/skills"
+import { skills } from "../../../data/skills";
 
 // npm modules
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 // components
-import SkillContainer from "../SkillContainer/SkillContainer"
+import SkillContainer from "../SkillContainer/SkillContainer";
 
 // mui components
-import Box from "@mui/material/Box"
-import Typography from "@mui/material/Typography"
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 const Skills = (props) => {
-  const { checked } = props
-  const [value, setValue] = useState('one');
-  const [tabs, setTabs] = useState(false)
+  const { checked } = props;
+  const [value, setValue] = useState("one");
+  const [tabs, setTabs] = useState(false);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 900) {
-        setTabs(false)
+        setTabs(false);
       } else if (window.innerWidth <= 899) {
-        setTabs(true)
+        setTabs(true);
       }
-    }
+    };
 
-    handleResize()
+    handleResize();
 
-    window.addEventListener('resize', handleResize)
+    window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener('resize', handleResize)
-
-  }, [])
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <>
-      <Typography variant="h4" sx={{  pt: '5vh' }}>
+      <Typography variant="h4" sx={{ pt: "5vh" }}>
         Skills
       </Typography>
-      <Box sx={{ pt: 4, display: {sm: 'block', md: 'none'} }}>
+      <Box sx={{ pt: 4, display: { sm: "block", md: "none" } }}>
         <Tabs
           value={value}
           onChange={handleChange}
@@ -53,48 +52,48 @@ const Skills = (props) => {
         >
           <Tab
             value="one"
-            label="frontend"
-            sx={{ color: checked ? '#fff' : '#000' }}
+            label="primary stack"
+            sx={{ color: checked ? "#fff" : "#000" }}
           />
           <Tab
             value="two"
-            label="backend"
-            sx={{ color: checked ? '#fff' : '#000' }}
-            />
+            label="languages"
+            sx={{ color: checked ? "#fff" : "#000" }}
+          />
           <Tab
             value="three"
-            label="other"
-            sx={{ color: checked ? '#fff' : '#000' }}
+            label="cloud"
+            sx={{ color: checked ? "#fff" : "#000" }}
           />
         </Tabs>
       </Box>
       <Box
         sx={{
-          textAlign: 'center',
-          display: 'flex',
+          textAlign: "center",
+          display: "flex",
         }}
       >
         <SkillContainer
           tabs={tabs}
           value={value}
-          tabValue='one'
-          tech={skills.map(tech => tech.frontend)}
+          tabValue="one"
+          tech={skills.map((tech) => tech.primaryStack)}
         />
         <SkillContainer
           tabs={tabs}
           value={value}
-          tabValue='two'
-          tech={skills.map(tech => tech.backend)}
+          tabValue="two"
+          tech={skills.map((tech) => tech.languages)}
         />
         <SkillContainer
           tabs={tabs}
           value={value}
-          tabValue='three'
-          tech={skills.map(tech => tech.other)}
+          tabValue="three"
+          tech={skills.map((tech) => tech.cloud)}
         />
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,110 +1,65 @@
 export const skills = [
   {
-    frontend: [
+    primaryStack: [
       {
-        name: 'React',
-        devIcon: 'devicon-react-original colored',
-        docs: 'https://legacy.reactjs.org/docs/getting-started.html',
+        name: "JavaScript",
+        devIcon: "devicon-javascript-plain colored",
+        docs: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
       },
       {
-        name: 'Material UI',
-        devIcon: 'devicon-materialui-plain colored',
-        docs: 'https://mui.com/material-ui/getting-started/overview/',
+        name: "TypeScript",
+        devIcon: "devicon-typescript-plain colored",
+        docs: "https://www.typescriptlang.org/docs/",
       },
       {
-        name: 'TypeScript',
-        devIcon: 'devicon-typescript-plain colored',
-        docs: 'https://www.typescriptlang.org/docs/',
+        name: "React",
+        devIcon: "devicon-react-original colored",
+        docs: "https://legacy.reactjs.org/docs/getting-started.html",
       },
       {
-        name: 'JavaScript',
-        devIcon: 'devicon-javascript-plain colored',
-        docs: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+        name: "Node.js",
+        devIcon: "devicon-nodejs-plain colored",
+        docs: "https://nodejs.org/en/docs",
       },
       {
-        name: 'CSS',
-        devIcon: 'devicon-css3-plain colored',
-        docs: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+        name: "Express",
+        devIcon: "devicon-express-original colored",
+        docs: "https://expressjs.com/",
       },
       {
-        name: 'HTML',
-        devIcon: 'devicon-html5-plain colored',
-        docs: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+        name: "MongoDB",
+        devIcon: "devicon-mongodb-plain colored",
+        docs: "https://www.mongodb.com/docs/",
       },
       {
-        name: 'EJS',
-        img: 'https://i.imgur.com/HryZXxf.png',
-        docs: 'https://ejs.co/',
-      },
-    ],
-    backend: [
-      {
-        name: 'Node',
-        devIcon: 'devicon-nodejs-plain colored',
-        docs: 'https://nodejs.org/en/docs',
-      },
-      {
-        name: 'Express',
-        devIcon: 'devicon-express-original colored',
-        docs: 'https://expressjs.com/',
-      },
-      {
-        name: 'PostgreSQL',
-        devIcon: 'devicon-postgresql-plain colored',
-        docs: 'https://www.postgresql.org/docs/',
-      },
-      {
-        name: 'Sequelize',
-        devIcon: 'devicon-sequelize-plain colored',
-        docs: 'https://sequelize.org/',
-      },
-      {
-        name: 'MongoDB',
-        devIcon: 'devicon-mongodb-plain colored',
-        docs: 'https://www.mongodb.com/docs/',
-      },
-      {
-        name: 'Mongoose',
-        img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongoose/mongoose.png',
-        docs: 'https://mongoosejs.com/docs/',
-      },
-      {
-        name: 'AWS S3',
-        devIcon: 'devicon-amazonwebservices-plain-wordmark colored',
-        docs: 'https://docs.aws.amazon.com/s3/',
+        name: "PostgreSQL",
+        devIcon: "devicon-postgresql-plain colored",
+        docs: "https://www.postgresql.org/docs/",
       },
     ],
-    other: [
+    languages: [
       {
-        name: 'NPM',
-        devIcon: 'devicon-npm-original-wordmark colored',
-        docs: 'https://www.npmjs.com/package/npm-docs',
+        name: "Python",
+        devIcon: "devicon-python-plain colored",
+        docs: "https://docs.python.org/3/",
       },
       {
-        name: 'REST APIs',
-        img: 'https://keenethics.com/wp-content/uploads/2022/01/rest-api-1.svg',
-        docs: 'https://developer.mozilla.org/en-US/docs/Glossary/REST'
+        name: "Java",
+        devIcon: "devicon-java-plain colored",
+        docs: "https://docs.oracle.com/en/java/",
       },
       {
-        name: 'MVC Architecture',
-        img: 'https://cdn2.iconfinder.com/data/icons/programming-76/512/MVC-model-view-controller-512.png',
-        docs: 'https://developer.mozilla.org/en-US/docs/Glossary/MVC',
-      },
-      {
-        name: 'Environment Variables',
-        img: 'https://cdn-1.webcatalog.io/catalog/dotenv/dotenv-icon-filled-256.png',
-        docs: 'https://www.npmjs.com/package/dotenv',
-      },
-      {
-        name: 'JWT Auth',
-        img: 'https://img.icons8.com/color/512/java-web-token.png',
-        docs: 'https://jwt.io/introduction',
-      },
-      {
-        name: 'Google OAuth',
-        img: 'https://registry.npmmirror.com/@lobehub/icons-static-png/1.18.0/files/dark/google-color.png',
-        docs: 'https://developers.google.com/identity/protocols/oauth2',
+        name: "C++",
+        devIcon: "devicon-cplusplus-plain colored",
+        docs: "https://en.cppreference.com/w/cpp",
       },
     ],
-  }
-]
+    cloud: [
+      {
+        name: "Amazon S3",
+        devIcon: "devicon-amazonwebservices-plain-wordmark colored",
+        docs: "https://docs.aws.amazon.com/s3/",
+      },
+    ],
+  },
+];
